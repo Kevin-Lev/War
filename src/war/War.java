4dff5c91a95a8c21fe72603c32e2f8b35c30dda0
Kevin-Lev <kevinlevrone.r@gmail.com>
2017-02-01 12:43:36 -0200

    Distribuição dos Territórios aos jogadores

--- src/war/War.java
+++ src/war/War.java
@@ -24,47 +24,49 @@ public class War {
     public static void main(String[] args) {
         // TODO code application logic here
         List<Territorio> Total_ter = new ArrayList();
+        
 /////////////////////// América do Norte////////////////////////////////
+
+        Continente.continentes.AFRICA.setNum_territorios(6);
+        Continente.continentes.AMERICA_DO_NORTE.setNum_territorios(7);
+        Continente.continentes.AMERICA_DO_SUL.setNum_territorios(4);
+        Continente.continentes.ASIA.setNum_territorios(7);
+        Continente.continentes.EUROPA.setNum_territorios(5);
+        Continente.continentes.OCEANIA.setNum_territorios(4);
+
 
         Territorio al = new Territorio();
         al.setNome("Alasca");
-        al.setCor("amarelo");
         al.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(al);
         
         Territorio van = new Territorio();
         van.setNome("Vancouver");
-        van.setCor("amarelo");
         van.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(van);
         
         Territorio cal = new Territorio();
         cal.setNome("California");
-        cal.setCor("amarelo");
         cal.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(cal);
         
         Territorio mex = new Territorio();
         mex.setNome("Mexico");
-        mex.setCor("amarelo");
         mex.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(mex);
         
         Territorio gro = new Territorio();
         gro.setNome("Groenlandia");
-        gro.setCor("amarelo");
         gro.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(gro);
         
         Territorio ota = new Territorio();
         ota.setNome("Otawa");
-        ota.setCor("amarelo");
         ota.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(ota);
         
         Territorio ny = new Territorio();
         ny.setNome("Nova York");
-        ny.setCor("amarelo");
         ny.setContinente(Continente.continentes.AMERICA_DO_NORTE);
         Total_ter.add(ny);
         
@@ -72,25 +74,21 @@ public class War {
         
         Territorio chi = new Territorio();
         chi.setNome("Chile");
-        chi.setCor("verde");
         chi.setContinente(Continente.continentes.AMERICA_DO_SUL);
         Total_ter.add(chi);
         
         Territorio col = new Territorio();
         col.setNome("Colombia");
-        col.setCor("verde");
         col.setContinente(Continente.continentes.AMERICA_DO_SUL);
         Total_ter.add(col);
         
         Territorio arg = new Territorio();
         arg.setNome("Argentina");
-        arg.setCor("verde");
         arg.setContinente(Continente.continentes.AMERICA_DO_SUL);
         Total_ter.add(arg);
         
         Territorio bra = new Territorio();
         bra.setNome("Brasil");
-        bra.setCor ("verde");
         bra.setContinente(Continente.continentes.AMERICA_DO_SUL);
         Total_ter.add(bra);
         
@@ -98,31 +96,26 @@ public class War {
         
         Territorio  ing = new Territorio();
         ing.setNome("Inglaterra");
-        ing.setCor("azul");
         ing.setContinente(Continente.continentes.EUROPA);
         Total_ter.add(ing);
         
         Territorio ita = new Territorio();
         ita.setNome("Italia");
-        ita.setCor("azul");
         ita.setContinente(Continente.continentes.EUROPA);
         Total_ter.add(ita);
         
         Territorio sue = new Territorio();
         sue.setNome("Suécia");
-        sue.setCor("azul");
         sue.setContinente(Continente.continentes.EUROPA);
         Total_ter.add(sue);
         
         Territorio ale = new Territorio();
         ale.setNome("Alemanha");
-        ale.setCor("azul");
         ale.setContinente(Continente.continentes.EUROPA);
         Total_ter.add(ale);
         
         Territorio mos = new Territorio();
         mos.setNome("Moscou");
-        mos.setCor("azul");
         mos.setContinente(Continente.continentes.EUROPA);
         Total_ter.add(mos);
         
@@ -130,37 +123,31 @@ public class War {
         
         Territorio nig = new Territorio();
         nig.setNome("Nigeria");
-        nig.setCor("roxo");
         nig.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(nig);
         
         Territorio egi = new Territorio();
         egi.setNome("Egito");
-        egi.setCor("roxo");
         egi.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(egi);
         
         Territorio con = new Territorio();
         con.setNome("Congo");
-        con.setCor("roxo");
         con.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(con);
         
         Territorio afr = new Territorio();
         afr.setNome("África do Sul");
-        afr.setCor("roxo");
         afr.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(afr);
         
         Territorio mad = new Territorio();
         mad.setNome("Madagascar");
-        mad.setCor("roxo");
         mad.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(mad);
         
         Territorio sud = new Territorio();
         sud.setNome("Sudão");
-        sud.setCor("roxo");
         sud.setContinente(Continente.continentes.AFRICA);
         Total_ter.add(sud);
         
@@ -168,43 +155,36 @@ public class War {
         
         Territorio ori = new Territorio();
         ori.setNome("Oriente Médio");
-        ori.setCor("laranja");
         ori.setContinente(Continente.continentes.ASIA);
         Total_ter.add(ori);
         
         Territorio ind = new Territorio();
         ind.setNome("Índia");
-        ind.setCor("laranja");
         ind.setContinente(Continente.continentes.ASIA);
         Total_ter.add(ind);
         
         Territorio om = new Territorio();
         om.setNome("Omsk");
-        om.setCor("laranja");
         om.setContinente(Continente.continentes.ASIA);
         Total_ter.add(om);
         
         Territorio vlad = new Territorio();
         vlad.setNome("Vladivostok");
-        vlad.setCor("laranja");
         vlad.setContinente(Continente.continentes.ASIA);
         Total_ter.add(vlad);
         
         Territorio sib = new Territorio();
         sib.setNome("Sibéria");
-        sib.setCor("laranja");
         sib.setContinente(Continente.continentes.ASIA);
         Total_ter.add(sib);
         
         Territorio chin = new Territorio();
         chin.setNome("China");
-        chin.setCor("laranja");
         chin.setContinente(Continente.continentes.ASIA);
         Total_ter.add(chin);
         
         Territorio jap = new Territorio();
         jap.setNome("Japão");
-        jap.setCor("laranja");
         jap.setContinente(Continente.continentes.ASIA);
         Total_ter.add(jap);
         
@@ -212,260 +192,284 @@ public class War {
         
         Territorio sum = new Territorio();
         sum.setNome("Sumatra");
-        sum.setCor("rosa");
         sum.setContinente(Continente.continentes.OCEANIA);
         Total_ter.add(sum);
         
         Territorio bor = new Territorio();
         bor.setNome("Bornéu");
-        bor.setCor("rosa");
         bor.setContinente(Continente.continentes.OCEANIA);
         Total_ter.add(bor);
         
         Territorio aus = new Territorio();
         aus.setNome("Austrália");
-        aus.setCor("rosa");
         aus.setContinente(Continente.continentes.OCEANIA);
         Total_ter.add(aus);
         
         Territorio nov = new Territorio();
         nov.setNome("Nova Guiné");
-        nov.setCor("rosa");
         nov.setContinente(Continente.continentes.OCEANIA);
         Total_ter.add(nov);
  
  //////////////// Definição das fronteiras /////////////////////////////////////       
 // América do Norte
-    List<Territorio> t = new ArrayList();
-        t.add(van);
-        t.add(vlad);
-        al.setFronteira(t);
-        t.clear();
         
-        t.add(al);
-        t.add(cal);
-        t.add(gro);
-        van.setFronteira(t);
-        t.clear();
+    /*    al.setFronteira(van);
+        al.setFronteira(vlad);
         
-        t.add(van);
-        t.add(ota);
-        t.add(ing);
-        gro.setFronteira(t);
-        t.clear();
         
-        t.add(van);
-        t.add(ota);
-        t.add(mex);
-        cal.setFronteira(t);
-        t.clear();
+        van.setFronteira(al);
+        van.setFronteira(cal);
+        van.setFronteira(gro);
         
-        t.add(cal);
-        t.add(gro);
-        t.add(ny);
-        ota.setFronteira(t);
-        t.clear();
+      
+        gro.setFronteira(van);
+        gro.setFronteira(ota);
+        gro.setFronteira(ing);
         
-        t.add(cal);
-        t.add(ny);
-        t.add(col);
-        mex.setFronteira(t);
-        t.clear();
-    
+       
+        cal.setFronteira(van);
+        cal.setFronteira(ota);
+        cal.setFronteira(mex);
+        
+       
+        ota.setFronteira(cal);
+        ota.setFronteira(gro);
+        ota.setFronteira(ny);
+        
+        
+        mex.setFronteira(cal);
+        mex.setFronteira(ny);
+        mex.setFronteira(col);
+        
 // América do Sul
-        t.add(ota);
-        t.add(mex);
-        t.add(nig);
-        ny.setFronteira(t);
-        t.clear();
         
-        t.add(col);
-        t.add(arg);
-        t.add(jap);
-        chi.setFronteira(t);
-        t.clear();
+        ny.setFronteira(ota);
+        ny.setFronteira(mex);
+        ny.setFronteira(nig);
         
-        t.add(mex);
-        t.add(bra);
-        t.add(chi);
-        col.setFronteira(t);
-        t.clear();
         
-        t.add(chi);
-        t.add(bra);
-        t.add(nov);
-        arg.setFronteira(t);
-        t.clear();
+        chi.setFronteira(col);
+        chi.setFronteira(arg);
+        chi.setFronteira(jap);
+        
+       
+        col.setFronteira(mex);
+        col.setFronteira(bra);
+        col.setFronteira(chi);
+        
+       
+        arg.setFronteira(chi);
+        arg.setFronteira(bra);
+        arg.setFronteira(nov);
+        
         
 //Europa
-        t.add(gro);
-        t.add(ita);
-        ing.setFronteira(t);
-        t.clear();
         
-        t.add(ing);
-        t.add(ale);
-        t.add(sue);
-        ita.setFronteira(t);
-        t.clear();
+        ing.setFronteira(gro);
+        ing.setFronteira(ita);
+       
         
-        t.add(ita);
-        t.add(mos);
-        t.add(egi);
-        ale.setFronteira(t);
-        t.clear();
         
-        t.add(ita);
-        t.add(mos);
-        sue.setFronteira(t);
-        t.clear();
+        ita.setFronteira(ing);
+        ita.setFronteira(ale);
+        ita.setFronteira(sue);
+    
         
-        t.add(sue);
-        t.add(om);
-        t.add(ori);
-        t.add(ale);
-        mos.setFronteira(t);
-        t.clear();
+       
+        ale.setFronteira(ita);
+        ale.setFronteira(mos);
+        ale.setFronteira(egi);
+        
+       
+        sue.setFronteira(ita);
+        sue.setFronteira(mos);
+        
+        
+        mos.setFronteira(sue);
+        mos.setFronteira(om);
+        mos.setFronteira(ori);
+        mos.setFronteira(ale);
 
 // África
-        t.add(ny);
-        t.add(con);
-        t.add(egi);
-        nig.setFronteira(t);
-        t.clear();
         
-        t.add(ale);
-        t.add(nig);
-        t.add(sud);
-        t.add(ori);
-        egi.setFronteira(t);
-        t.clear();
+               
+       
+        egi.setFronteira(ale);
+        egi.setFronteira(nig);
+        egi.setFronteira(sud);
+        egi.setFronteira(ori);
         
-        t.add(ota);
-        t.add(mex);
-        con.setFronteira(t);
-        t.clear();
         
-        t.add(egi);
-        t.add(sum);
-        t.add(con);
-        t.add(mad);
-        sud.setFronteira(t);
-        t.clear();
+        con.setFronteira(ota);
+        con.setFronteira(mex);
         
-        t.add(con);
-        t.add(mad);
-        afr.setFronteira(t);
-        t.clear();
         
-        t.add(sud);
-        t.add(afr);
-        mad.setFronteira(t);
-        t.clear();
+        sud.setFronteira(egi);
+        sud.setFronteira(sum);
+        sud.setFronteira(con);
+        sud.setFronteira(mad);
+        
+        
+        afr.setFronteira(con);
+        afr.setFronteira(mad);
+        
+        
+        mad.setFronteira(sud);
+        mad.setFronteira(afr);
         
 // Ásia
-        t.add(al);
-        t.add(sib);
-        vlad.setFronteira(t);
-        t.clear();
         
-        t.add(mos);
-        t.add(sib);
-        t.add(ind);
-        om.setFronteira(t);
-        t.clear();
+        vlad.setFronteira(al);
+        vlad.setFronteira(sib);
         
-        t.add(vlad);
-        t.add(om);
-        t.add(chi);
-        sib.setFronteira(t);
-        t.clear();
         
-        t.add(mos);
-        t.add(egi);
-        t.add(ind);
-        t.add(sum);
-        ori.setFronteira(t);
-        t.clear();
+        om.setFronteira(mos);
+        om.setFronteira(sib);
+        om.setFronteira(ind);
         
-        t.add(om);
-        t.add(ori);
-        t.add(chi);
-        t.add(bor);
-        ind.setFronteira(t);
-        t.clear();
         
-        t.add(sib);
-        t.add(ind);
-        t.add(jap);
-        chi.setFronteira(t);
-        t.clear();
+        sib.setFronteira(vlad);
+        sib.setFronteira(om);
+        sib.setFronteira(chi);
         
-        t.add(bor);
-        t.add(nov);
-        t.add(chi);
-        jap.setFronteira(t);
-        t.clear();
+        
+        ori.setFronteira(mos);
+        ori.setFronteira(egi);
+        ori.setFronteira(ind);
+        ori.setFronteira(sum);
+      
+       
+        ind.setFronteira(om);
+        ind.setFronteira(ori);
+        ind.setFronteira(chi);
+        ind.setFronteira(bor);
+        
+       
+        chi.setFronteira(sib);
+        chi.setFronteira(ind);
+        chi.setFronteira(jap);
+        
+       
+        jap.setFronteira(bor);
+        jap.setFronteira(nov);
+        jap.setFronteira(chi);
         
 // Oceania
-        t.add(sud);
-        t.add(bor);
-        t.add(ori);
-        sum.setFronteira(t);
-        t.clear();
         
-        t.add(ind);
-        t.add(sum);
-        t.add(jap);
-        t.add(aus);
-        bor.setFronteira(t);
-        t.clear();
+        sum.setFronteira(sud);
+        sum.setFronteira(bor);
+        sum.setFronteira(ori);
         
-        t.add(bor);
-        t.add(nov);
-        aus.setFronteira(t);
-        t.clear();
         
-        t.add(jap);
-        t.add(aus);
-        t.add(arg);
-        nov.setFronteira(t);
-        t.clear();
+        bor.setFronteira(ind);
+        bor.setFronteira(sum);
+        bor.setFronteira(jap);
+        bor.setFronteira(aus);
+     
+        
+        aus.setFronteira(bor);
+        aus.setFronteira(nov);
+        
+        
+        nov.setFronteira(jap);
+        aus.setFronteira(aus);
+        aus.setFronteira(arg); */
+        
   
-    
  /////////////////////// Inicialização//////////////////////////////////////////
     
         Jogador jogador_1 = new Jogador();
-        Jogador jogador_2 = new Jogador();
-        int i=0,cont=0;
+        Jogador adversario = new Jogador();
+        int i=0,j,cont=0;
+        
+        System.out.println("####################### WAR ##########################");
+        System.out.println("\n");
         
         Collections.shuffle(Total_ter); // Para dar um Random nas posições dos elementos da lista
         jogador_1.setNum(0);
         jogador_1.setCor("Azul");
-        jogador_2.setNum(1);
-        jogador_2.setCor("Vermelho");
+        adversario.setNum(1);
+        adversario.setCor("Vermelho");
         if(jogador_1.getNum() == 1){
             System.out.println("O jogador 1 terá um território a mais");
         }
         else{
-            System.out.println("O jogador 2 terá um território a mais");
+            System.out.println("O adversário terá um território a mais");
         }
         
         System.out.println("\n");
         
+        Territorio[][] mapa = new Territorio[5][8];
+        
+        mapa[0][0] = al;    
+        mapa[0][1] = van;
+        mapa[0][2] = gro;
+        mapa[0][3] = ing;
+        mapa[0][4] = ita;
+        mapa[0][5] = sue;
+        mapa[0][7] = vlad;
+        mapa[1][1] = cal;
+        mapa[1][2] = ota;
+        mapa[1][4] = ale;
+        mapa[1][5] = mos;
+        mapa[1][6] = om;
+        mapa[1][7] = sib;
+        mapa[2][1] = mex;
+        mapa[2][2] = cal;
+        mapa[2][3] = nig;
+        mapa[2][4] = egi;
+        mapa[2][5] = ori;
+        mapa[2][6] = ind;
+        mapa[2][7] = chin;
+        mapa[3][0] = chi;
+        mapa[3][1] = col;
+        mapa[3][3] = con;
+        mapa[3][4] = sud;
+        mapa[3][5] = sum;
+        mapa[3][6] = bor;
+        mapa[3][7] = jap;
+        mapa[4][0] = arg;
+        mapa[4][1] = bra;
+        mapa[4][3] = afr;
+        mapa[4][4] = mad;
+        mapa[4][6] = aus;
+        mapa[4][7] = nov;
+        
         for(Territorio ter : Total_ter){
             i++;
             if(i>=17){
-              System.out.println("Território do jogador 2: \n");
-              ter.setCor(jogador_2.getCor());
-              System.out.println("Território " + ter.getNome() + " da cor: " + ter.getCor() + "\n");
+              ter.setCor(adversario.getCor());
+              ter.setTerrestre(1);
+              ter.setAereo(1);
             }
             else{
-              System.out.println("Território do jogador 1: \n");
               ter.setCor(jogador_1.getCor());
-              System.out.println("Território " + ter.getNome() + " da cor: " + ter.getCor() + "\n");
+              ter.setTerrestre(1);
+              ter.setAereo(1);
             }
         }
+        
+        System.out.println("-------------DISTRIBUIÇÃO DE TERRITÓRIOS-------------");
+        System.out.println("\n");
+        
+        for(i=0;i<5;i++){
+            for(j=0;j<8;j++){
+                if(mapa[i][j] == null){
+                    System.out.print("VAZIO \t\t\t\t\t\t");
+                }
+                else if("Azul".equals(mapa[i][j].getCor())){
+                    System.out.print("|" + mapa[i][j].getNome() + "[Terrestre = " + mapa[i][j].getTerrestre() + "]" + "[Aéreo = " + mapa[i][j].getAereo() + "]" + "[Azul]" + "|" + "\t");
+                }
+                else{
+                    System.out.print("|" + mapa[i][j].getNome() + "[Terrestre = " + mapa[i][j].getTerrestre() + "]" + "[Aéreo = " + mapa[i][j].getAereo() + "]" + "[Vermelho]" + "|" + "\t");
+                }
+            }
+            System.out.println("\n\n");
+        }
+        
+        
+        
+    //////////////////////////////// Preparação ////////////////////////////////    
     
     }  
 }   
