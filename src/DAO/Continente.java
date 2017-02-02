7df90c5b7e311b584700dbc4143ca5a3a5d42da7
Alpaca-kun <ra90558@uem.br>
2017-02-02 17:00:14 -0200

    Testando funcoes do git

--- src/DAO/Continente.java
+++ src/DAO/Continente.java
@@ -12,8 +12,8 @@ package DAO;
 public class Continente {
     
     public enum continentes{
-        AFRICA,AMERICA_DO_NORTE, AMERICA_DO_SUL, ASIA, EUROPA, OCEANIA;
-        
+        AFRICA, AMERICA_DO_NORTE, AMERICA_DO_SUL, ASIA, EUROPA, OCEANIA;
+         
         private int num_territorios;
 
         public void setNum_territorios(int num_territorios) {
