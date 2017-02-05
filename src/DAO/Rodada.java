99fa0ce3f69a394e46206ef87b28e494aa119904
Kevin-Lev <kevinlevrone.r@gmail.com>
2017-02-05 17:34:19 -0200

    Função combateTerrestre() quase finalizada

--- src/DAO/Rodada.java
+++ src/DAO/Rodada.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import war.War;
 
 /**
  *
@@ -17,31 +18,60 @@ import java.util.Scanner;
 public class Rodada {
     
     public static void combateTerrestre(Jogador atacante, Jogador defesa){
-       Map<Integer, String> fronteirasdadefesa = new HashMap();
-       Map<Integer, String> territoriospraataque = new HashMap();
+       Map<Integer, Territorio> fronteirasdadefesa = new HashMap();
+       Map<Integer, Territorio> territoriospraataque = new HashMap();
        List<Territorio> territoriosatacante = atacante.getTerritorios();
        Scanner scanner = new Scanner(System.in); 
        int i=0, l;
        
        for(Territorio t: territoriosatacante){
           if(t.getListaterrestre().size()>1){
-              territoriospraataque.put(i, t.getNome());
+              territoriospraataque.put(i, t);
               i++;
           } 
        }
        
        System.out.println(atacante.getNome() + " - Selecione o número de um dos seus territórios conquistados: \n");
        for(Integer key: territoriospraataque.keySet()){
-           System.out.println(key + ")" + territoriospraataque.get(key));
+           System.out.println(key + ")" + territoriospraataque.get(key).getNome());
        }
        System.out.print("Território: ");
        l = scanner.nextInt();
        while(territoriospraataque.get(l)==null){
-           System.out.println("Selecione um número válido: ");
+           System.out.print("Selecione um número válido: ");
            l = scanner.nextInt();
        }
        
+       Territorio ter_ataque = territoriospraataque.get(l);
        
+       for(Territorio t: territoriospraataque.get(l).getFronteira()){
+          if(t.getCor().equals(defesa.getCor())){ 
+           fronteirasdadefesa.put(i, t);
+           i++;
+          }
+       }
+       
+        System.out.println(atacante.getNome() + "- Selecione a fronteira inimiga para atacar!\n");
+        for(Integer key: fronteirasdadefesa.keySet()){
+            System.out.println(key + ")" + " " + fronteirasdadefesa.get(key).getNome()); 
+        }
+        System.out.print("Fronteira: ");
+        l = scanner.nextInt();
+        while(territoriospraataque.get(l)==null){
+           System.out.print("Selecione um número válido: ");
+           l = scanner.nextInt();
+        }
+        
+        Territorio ter_defesa = fronteirasdadefesa.get(l);
+        
+        System.out.println("Deseja atacar " + ter_defesa.getNome() + " com quantos exércitos de " + ter_ataque + "?\n");
+        System.out.println("Quantidade de exércitos disponíveis em " + ter_ataque.getNome() + " : " + ter_ataque.getListaterrestre() + "\n");
+        System.out.print("Exércitos: ");
+        l = scanner.nextInt();
+        while(l>=ter_ataque.getListaterrestre().size()){
+            System.out.print("Valor inválido! Deve permanecer pelo menos 1 exército no território");
+            l = scanner.nextInt();
+        }
     }
     
     public static void selecionaOpcao(Jogador atacante, Jogador defesa){
